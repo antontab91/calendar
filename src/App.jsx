40535e97690e9ad593af2408c440c6e8698634e1
Milkweedd <antontab91@gmail.com>
2020-07-01 21:25:12 +0300

    as

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import Calendar from "./Calendar";
 
 const App = () => {
-  return <Calendar />;
+  return (
+    <div className="sa">44542</div>
+  );
 };
 
 export default App;
