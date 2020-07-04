ba65292fdf7d2519c6ac3629dfcd53eeb4e3d198
Milkweedd <antontab91@gmail.com>
2020-07-05 01:26:09 +0300

    sa

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,17 +1,9 @@
 import React from 'react';
 
-import AddBtn from './AddBtn';
-import Navigation from './Navigation';
-import CurrentDate from './CurrentDate';
-
-
-
 const Header = () => {
     return (
-        <div className="header">
-            <AddBtn prop1={} prop2={} />
-            <Navigation prop1={} prop2={} />
-            <CurrentDate prop1={} prop2={} />
-        </div>
+        
     )
 }
+
+export default Header;
