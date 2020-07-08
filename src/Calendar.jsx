4a6a599208d6dbd831f9dbe40fbd13b5abd2cfdb
Milkweedd <antontab91@gmail.com>
2020-07-08 03:24:49 +0300

    фі

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -3,26 +3,44 @@ import Header from './Header/Header.jsx';
 import Main from './Main/Main.jsx';
 import Popup from './Poppup/Poppup.jsx';
 
+
+// Algo getMonday
+// 1 отталкиваемся от теущей даты - записываем ее в переменную тудей , обьявляем переменну мандей , куда будем записывать дату понедельника 
+// 2 перебераем неделю по индексам (начинается с 0 (неделя по американски начинается с вс))
+// 3 если индекс дня не равен 1 (понедельнику ) , записываем тудей новую дату на день меньше , пока не дойдем до нужного индекса 
+// 4 если индекс равен 1 - это понедельник ,то что нужно 
+
+
+export const getMonday = () => {
+  const today = new Date();
+  let monday = null;
+  for (let i = 0; i < 7; i++) {
+    if (today.getDay() !== 1) {
+      today.setDate(today.getDate() - 1);
+    } else {
+      monday = today;
+    }
+  }
+  return monday;
+};
+
+
 class Calendar extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
       today: new Date(),
-      shownPopup: false,
-      zerroDay: 0,
+      monday: getMonday(),
     }
   }
-
-  // getMonday = () => {
-
-  // }
-
 
   render() {
     return (
       <div className="calendar">
-        <Header />
+        <Header
+
+        />
         <Main />
         <Popup />
       </div>
