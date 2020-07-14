8a1e638e328fe568baf6cb5b804b3fd766c061e6
Milkweedd <antontab91@gmail.com>
2020-07-15 01:01:40 +0300

    ыф

--- src/Main/Sidebar/Sidebar.jsx
+++ src/Main/Sidebar/Sidebar.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import moment from 'moment';
-
+import './sidebar.scss'
 import { getRangeOfHoursByDate } from '../../utilites';
 
-const hours = getRangeOfHoursByDate(new Date());
+const hours = getRangeOfHoursByDate(new Date()); // массив часов , если это читает Андрей или Вася , я себе все это пишу в коментах чтобы не запутаться . пс понимаю что на реальных проэктах такой срач не допустим 
 
 class Sidebar extends React.Component {
   render() {
@@ -12,7 +12,9 @@ class Sidebar extends React.Component {
       <aside className="sidebar">
         {hours.map((hour, index) => {
           return (<div className="sidebar__hour" key={hour}>
-            <span className="sidebar__hour-text">{index === 0 ? null : moment(hour).format("HH:mm")}</span>
+            <span className="sidebar__hour-text">
+              {index === 0 ? null : moment(hour).format("HH:mm")}
+            </span>
           </div>)
         })}
       </aside>
