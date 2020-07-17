1a35063536b16263c1833654cb492c354d4383e8
Milkweedd <antontab91@gmail.com>
2020-07-17 17:41:01 +0300

    as

--- src/Main/Hour/Hour.jsx
+++ src/Main/Hour/Hour.jsx
@@ -1,41 +1,31 @@
 import React from 'react';
 import moment from 'moment';
-
 import RedLine from '../RedLine/RedLine';
 import Event from '../Event/Event';
-// import './hour.scss';
 
 
-class Hour extends React.Component {
-  render() {
-    const { hour, currDate, events } = this.props;
+const Hour = ({ hour, currDate, events }) => {
+  const mHour = moment(hour);
+  const isActive = mHour.isSame(currDate, 'hour');
 
-    const mHour = moment(hour);
+  return (
+    <div className="table__hour" >
 
-    const isActive = mHour.isSame(currDate, 'hour');
+      {
+        isActive ?
+          <RedLine hour={hour} currDate={currDate} />
+          : null
+      }
 
-    // if (events.length > 0) {
-    //   console.log(events)
-    // }
+      {
+        events.map((event) => {
+          return <Event key={event.id} event={event} hour={hour} />
+        })
+      }
 
-    // const diff = moment(currDate).diff(hour, 'minutes');
+    </div>
+  )
 
-    // const percentOfHour = (diff / 60 * 100).toFixed(0);
-
-    return (
-      <div className="table__hour" >
-
-        {
-          isActive ? <RedLine hour={hour} currDate={currDate} />
-            : null}
-
-        {events.map(event => {
-          return (<Event key={event.id} event={event} hour={hour} />
-          )
-        })}
-      </div>
-    )
-  }
 }
 
 export default Hour;
