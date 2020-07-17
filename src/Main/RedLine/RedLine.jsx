15db0d13d3bd08de59928abc08b1537db83522d8
Milkweedd <antontab91@gmail.com>
2020-07-17 17:45:06 +0300

    sa

--- src/Main/RedLine/RedLine.jsx
+++ src/Main/RedLine/RedLine.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import moment from 'moment';
-
 import './redLine.scss';
 
 
 const RedLine = ({ hour, currDate }) => {
+
   const diff = moment(currDate).diff(hour, 'minutes');
   const percentOfHour = (diff / 60 * 100).toFixed(0);
 
--- src/Main/Sidebar/Sidebar.jsx
+++ src/Main/Sidebar/Sidebar.jsx
@@ -3,23 +3,22 @@ import moment from 'moment';
 import './sidebar.scss'
 import { getRangeOfHoursByDate } from '../../utilites';
 
-const hours = getRangeOfHoursByDate(new Date()); // массив часов 
 
-class Sidebar extends React.Component {
-  render() {
-    // если самый первый час (с 0 индексом - не отрисовуем (как в макете ))
-    return (
-      <aside className="sidebar">
-        {hours.map((hour, index) => {
-          return (<div className={`sidebar__hour ${index === 0 ? 'first' : ''}`} key={hour}>
-            <span className="sidebar__hour-text">
-              {index === 0 ? null : moment(hour).format("HH:mm")}
-            </span>
-          </div>)
-        })}
-      </aside>
-    )
-  }
+const Sidebar = () => {
+  const hours = getRangeOfHoursByDate(new Date()); // массив часов 
+
+  return (
+    <aside className="sidebar">
+      {hours.map((hour, index) => {
+        return (<div className={`sidebar__hour ${index === 0 ? 'first' : ''}`} key={hour}>
+          <span className="sidebar__hour-text">
+            {index === 0 ? null : moment(hour).format("HH:mm")}
+          </span>
+        </div>)
+      })}
+    </aside>
+  )
 }
+
 
 export default Sidebar;
