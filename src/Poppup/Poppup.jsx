34be96cfcf9af3181d8a940bb8b7da0c51eade99
Milkweedd <antontab91@gmail.com>
2020-07-17 17:49:28 +0300

    ds

--- src/Poppup/Poppup.jsx
+++ src/Poppup/Poppup.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './poppup.scss';
-import { faTimes, faCalendar } from "@fortawesome/free-solid-svg-icons";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
